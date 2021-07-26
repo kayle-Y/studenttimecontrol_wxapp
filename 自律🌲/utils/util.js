3a71efbf50958cbce854f944db16efc30f1f886c
kayle-Y <73332513+kayle-Y@users.noreply.github.com>
2021-07-26 13:30:41 +0800

    Add files via upload

--- 自律🌲/utils/util.js
+++ 自律🌲/utils/util.js
@@ -1,13 +1,4 @@
-// const formatTime = date => {
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate()
-//   const hour = date.getHours()
-//   const minute = date.getMinutes()
-//   const second = date.getSeconds()
 
-//   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-// }
 
 const formatDate = date =>{
   const year = date.getFullYear()
@@ -37,7 +28,6 @@ module.exports = {
  setSkin:setSkin
 
 }
-// module.exports = {
-//   formatTime: formatTime,
-//   formatDate: formatDate
-// }
+
+
+
